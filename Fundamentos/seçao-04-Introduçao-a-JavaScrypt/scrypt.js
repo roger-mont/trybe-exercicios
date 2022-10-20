24569612e031e5c54bd578f71abb0b3a2ce2b75b
roger-mont <roger_galvin@hotmail.com>
2022-10-20 17:22:33 -0400

    Exercicio area completo

--- Fundamentos/seçao-04-Introduçao-a-JavaScrypt/scrypt.js
+++ Fundamentos/seçao-04-Introduçao-a-JavaScrypt/scrypt.js
@@ -3,7 +3,7 @@ const birthCity = 'Manaus'
 let birthYear = 2004;
 
 birthYear = 2030;
-birthCity = 'Belem'
+//birthCity = 'Belem' //erro pq está tentando alterar uma constante//
 
 console.log(myName);
 console.log(birthCity);
