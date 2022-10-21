a72d60def038bdb0b6f142d1bac34cfe0c7e9ece
roger-mont <roger_galvin@hotmail.com>
2022-10-21 17:08:06 -0400

    segundo exercicio completo

--- Fundamentos/seçao-04-Introduçao-a-JavaScrypt/dia-02-JavaScrypt-Array-e-LoopFor/scrypt.js
+++ Fundamentos/seçao-04-Introduçao-a-JavaScrypt/dia-02-JavaScrypt-Array-e-LoopFor/scrypt.js
@@ -3,3 +3,8 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 //primerio exercicio//
 
 console.log(numbers[0]); console.log(numbers[1]); console.log(numbers[2]); console.log(numbers[3]); console.log(numbers[4]); console.log(numbers[5]); console.log(numbers[6]); console.log(numbers[7]); console.log(numbers[8]); console.log(numbers[9]);
+
+// segundo exercicio//
+
+let soma = numbers[0] + numbers[1] + numbers[2] + numbers[3] + numbers[4] + numbers[5] + numbers[6] + numbers[7] + numbers[8] + numbers[9];
+console.log(soma);
