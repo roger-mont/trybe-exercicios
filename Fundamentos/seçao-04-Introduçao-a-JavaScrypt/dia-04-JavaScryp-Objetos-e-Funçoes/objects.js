53c3839d40463d5d1e33b658e9e6d598c1cdeab4
roger-mont <roger_galvin@hotmail.com>
2022-10-25 17:50:38 -0400

    Segundo e terceiro requisito completo

--- Fundamentos/seçao-04-Introduçao-a-JavaScrypt/dia-04-JavaScryp-Objetos-e-Funçoes/objects.js
+++ Fundamentos/seçao-04-Introduçao-a-JavaScrypt/dia-04-JavaScryp-Objetos-e-Funçoes/objects.js
@@ -2,13 +2,26 @@ let leitor = {
   nome: 'Julia',
   sobrenome: 'Pessoa',
   idade: 21,
-  livrosFavoritos: {
+  livrosFavoritos: [
+    {
       titulo: 'O Pior Dia de Todos',
       autor: 'Daniela Kopsch',
       editora: 'Tordesilhas',
     },
+  ],
 };
 
 // primeiro requisito //
 
-console.log('O livro favorito de ' +leitor.nome + ' ' + leitor.sobrenome+ " é '" + leitor['livrosFavoritos']['titulo'] + "'");
+console.log('O livro favorito de ' +leitor.nome + ' ' + leitor.sobrenome+ " é '" + leitor.livrosFavoritos[0].titulo+ "'");
+ 
+// segundo requisito //
+leitor.livrosFavoritos.push(
+  {
+  titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+  autor: 'JK Rowling',
+  editor: 'Rocco',
+},
+)
+
+console.log('Julia tem ' +leitor.livrosFavoritos.length+ ' livros favoritos')
