c311a23ad7739906ddc1e1333a9c610a74338880
roger-mont <roger_galvin@hotmail.com>
2022-10-26 14:59:03 -0400

    Terceiro exercicio completo

---
+++ Fundamentos/seçao-04-Introduçao-a-JavaScrypt/dia-05-JavaScrypt-ES6-let-const-arrow-functions-e-template-literals/hello.js
@@ -0,0 +1,11 @@
+// function hello(nome) {
+//   return `Olá, ${nome}!`
+// }
+// let nome = 'Ivan';
+// console.log(hello(nome));
+
+//
+
+const hello = nome => `Olá ${nome}!`;
+let nome = 'Ivan';
+console.log(hello(nome));
